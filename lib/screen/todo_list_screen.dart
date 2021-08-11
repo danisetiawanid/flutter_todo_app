dd9b996d4bd8ac881298a78653687cddec7368ee
danisetiawanid <muhamaddanisetiawan@gmail.com>
2021-08-11 14:35:14 +0700

    downgrade sdk

--- lib/screen/todo_list_screen.dart
+++ lib/screen/todo_list_screen.dart
@@ -1,15 +1,31 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:todo_app/helpers/database_helper.dart';
+import 'package:todo_app/models/task_models.dart';
 import 'package:todo_app/screen/add_task_screen.dart';
 
 class TodoListScreen extends StatefulWidget {
-  const TodoListScreen({Key? key}) : super(key: key);
+  const TodoListScreen({Key key}) : super(key: key);
 
   @override
   _TodoListScreenState createState() => _TodoListScreenState();
 }
 
 class _TodoListScreenState extends State<TodoListScreen> {
+  Future<List<Task>> _taskList;
+
+  @override
+  void initState() {
+    super.initState();
+    _updateTaskList();
+  }
+
+  _updateTaskList() {
+    setState(() {
+      _taskList = DatabaseHelper.instance.getTaskList();
+    });
+  }
+
   Widget _buildTask(int index) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 25),
@@ -47,39 +63,50 @@ class _TodoListScreenState extends State<TodoListScreen> {
         },
         child: Icon(Icons.add),
       ),
-      body: ListView.builder(
-          padding: EdgeInsets.symmetric(vertical: 80),
-          itemCount: 10,
-          itemBuilder: (BuildContext contex, int index) {
-            if (index == 0) {
-              return Padding(
-                padding: EdgeInsets.symmetric(horizontal: 40, vertical: 20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'My Task',
-                      style: TextStyle(
-                          fontSize: 40,
-                          color: Colors.black,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Text(
-                      '1 of 10',
-                      style: TextStyle(
-                          color: Colors.grey,
-                          fontSize: 20,
-                          fontWeight: FontWeight.w600),
-                    )
-                  ],
-                ),
-              );
-            }
-            return _buildTask(index);
-          }),
+      body: FutureBuilder(
+        future: _taskList,
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return ListView.builder(
+            padding: EdgeInsets.symmetric(vertical: 80),
+            itemCount: 1 + snapshot.data.length,
+            itemBuilder: (BuildContext contex, int index) {
+              if (index == 0) {
+                return Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'My Task',
+                        style: TextStyle(
+                            fontSize: 40,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        '1 of 10',
+                        style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w600),
+                      )
+                    ],
+                  ),
+                );
+              }
+              return _buildTask(index);
+            },
+          );
+        },
+      ),
     );
   }
 }
