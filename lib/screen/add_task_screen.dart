e81c9964145693cc9b1bb0f1b6ca02de8999ba29
danisetiawanid <muhamaddanisetiawan@gmail.com>
2021-08-11 14:34:48 +0700

    downgrade sdk

--- lib/screen/add_task_screen.dart
+++ lib/screen/add_task_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 class AddTaskScreen extends StatefulWidget {
-  const AddTaskScreen({Key? key}) : super(key: key);
+  const AddTaskScreen({Key key}) : super(key: key);
 
   @override
   _AddTaskScreenState createState() => _AddTaskScreenState();
@@ -10,8 +10,8 @@ class AddTaskScreen extends StatefulWidget {
 
 class _AddTaskScreenState extends State<AddTaskScreen> {
   final _formKey = GlobalKey<FormState>();
-  String? _title = '';
-  String? _priority;
+  String _title = '';
+  String _priority;
   DateTime _date = DateTime.now();
   TextEditingController _dateController = TextEditingController();
 
@@ -31,7 +31,7 @@ class _AddTaskScreenState extends State<AddTaskScreen> {
   }
 
   _handleDatePicker() async {
-    final DateTime? date = await showDatePicker(
+    final DateTime date = await showDatePicker(
       context: context,
       initialDate: _date,
       firstDate: DateTime(2000),
@@ -46,8 +46,8 @@ class _AddTaskScreenState extends State<AddTaskScreen> {
   }
 
   _submit() {
-    if (_formKey.currentState!.validate()) {
-      _formKey.currentState!.save();
+    if (_formKey.currentState.validate()) {
+      _formKey.currentState.save();
       print('$_title, $_date, $_priority');
 
       // Insert the task to our user's database
@@ -100,7 +100,7 @@ class _AddTaskScreenState extends State<AddTaskScreen> {
                               borderRadius: BorderRadius.circular(10),
                             ),
                           ),
-                          validator: (input) => input!.trim().isEmpty
+                          validator: (input) => input.trim().isEmpty
                               ? 'Please enter a task title'
                               : null,
                           onSaved: (input) => _title = input,
@@ -153,7 +153,7 @@ class _AddTaskScreenState extends State<AddTaskScreen> {
                               : null,
                           onChanged: (value) {
                             setState(() {
-                              _priority = value as String?;
+                              _priority = value as String;
                             });
                           },
                           value: _priority,
